76f6c5795b96e839039fbf60f6c5fc3888fa4c94
Anas <anasmansoori734@gmail.com>
2020-11-12 20:31:54 +0530

    Rate us

--- app/src/main/java/binplus/foodiswill/Fragment/Payment_fragment.java
+++ app/src/main/java/binplus/foodiswill/Fragment/Payment_fragment.java
@@ -87,7 +87,7 @@ public class Payment_fragment extends Fragment {
     CheckBox checkBox_Wallet;
     CheckBox checkBox_coupon;
     EditText et_Coupon;
-    String getvalue;
+    String getvalue="Pay Now";
     String text;
     String deli_charges="";
     Dialog loadingBar;
@@ -966,7 +966,7 @@ public class Payment_fragment extends Fragment {
             intent.putExtra(AvenuesParams.ORDER_ID, ServiceUtility.chkNull(order_pay_id).toString().trim());
             intent.putExtra(AvenuesParams.CURRENCY, ServiceUtility.chkNull(getResources().getString(R.string.pay_currency)).toString().trim());
             intent.putExtra(AvenuesParams.AMOUNT, ServiceUtility.chkNull(total_amount).toString().trim());
-//            intent.putExtra(AvenuesParams.AMOUNT, ServiceUtility.chkNull("10").toString().trim());
+//            intent.putExtra(AvenuesParams.AMOUNT, ServiceUtility.chkNull("2").toString().trim());
             intent.putExtra(AvenuesParams.BILLING_NAME, ServiceUtility.chkNull(bill_name).toString().trim());
             intent.putExtra(AvenuesParams.BILLING_ADDRESS, ServiceUtility.chkNull(bill_address).toString().trim());
             intent.putExtra(AvenuesParams.BILLING_ZIP, ServiceUtility.chkNull(bill_pincode).toString().trim());
